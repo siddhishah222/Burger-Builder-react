69bf781b77c64c4137b141f941f9f48bcb8920ff
Siddhi Shah <58992453+siddhishah222@users.noreply.github.com>
2021-01-18 16:31:22 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,53 @@
-export { 
-          addIngredient,
-          removeIngredient,
-          initIngredients
-       } from './burgerBuilder';
-       
-export {
-          purchaseBurger,
-          purchasedInit,
-          fetchOrders
-      } from './order';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
+import burgerBuilderReducer from './store/reducer/burgerBuilder';
+import orderReducer from './store/reducer/order';
+import authReducer from './store/reducer/auth';
+import createSagaMiddleware from 'redux-saga';
+import {watchAuth, watchBurgerBuilder,watchOrder} from './store/sagas/index';
 
-export {
-    auth,
-    logout,
-    setAuthRedirectPath,
-    authCheckState
-} from './auth';
+// let composeEnhancers = null;
+// if (process.env.NODE_ENV === 'development') {
+//     composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// } else {
+//     composeEnhancers = compose;
+// }
+const composeEnhancers = (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) || compose;
+//const composeEnhancers = process.env.NODE_ENV==='development'? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__: null || compose;
+//process.env.NODE_ENV means only when you are in development mode you can inspect JS code, and all user in production mode cant access.
+const rootReducer=combineReducers({
+   burgerBuilder: burgerBuilderReducer,
+   order: orderReducer,
+   auth: authReducer //in Auth container in mapStateToProps state.auth.loading, where auth is the one defined here.
+})
 
+const sagaMiddleware=createSagaMiddleware();
 
-//only export async actionCreators
+//const store=createStore(burgerBuilderReducer,  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store=createStore(rootReducer, composeEnhancers(
+  applyMiddleware(thunk,sagaMiddleware)
+));
+
+sagaMiddleware.run(watchAuth);
+sagaMiddleware.run(watchBurgerBuilder);
+sagaMiddleware.run(watchOrder);
+
+const app=(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+);
+
+ReactDOM.render(
+    app, document.getElementById('root'));
+
+registerServiceWorker();
